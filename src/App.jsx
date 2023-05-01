fe52afb36a6eb5516bc9b478bc7db9e2ae77b9e2
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-05-01 18:56:07 +0530

    header added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-white text-gray-900 dark:bg-[#212936] dark:text-gray-300">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
